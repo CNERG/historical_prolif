e880607c931aa1c1b1de7d839b33d1d76f32e9c7
Meghan McGarry <mbmcgarry@wisc.edu>
2016-12-08 17:08:06 -0600

    writing functions to automatically compute factors and scores given raw data. Work in progress

---
+++ gen_fns.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+
+# Import data from a tab separated file
+def get_data(file, n_header=1, col_list=()):
+    row_names = np.genfromtxt(file, delimiter="\t", usecols=0, dtype=str,
+                            skip_header=n_header)
+
+    # Have to strip out the first column (of names) to get the rest of the info
+    tmp = np.genfromtxt(file, delimiter="\t", names=True,
+                        skip_header=n_header-1)
+    if not col_list:
+        col_list=range(1,len(tmp.dtype.names))
+
+    # now get the remaining column names (how is there not a better way?)
+    raw_data=np.genfromtxt(file, delimiter="\t", usecols=col_list,
+                      names=True, skip_header=n_header-1)
+    col_names=tmp.dtype.names
+
+#    raw_data=np.genfromtxt(file, delimiter="\t", usecols=col_list,
+#                           skip_header=n_header)
+
+    return row_names, col_names, raw_data
+
+# Make a numpy array of y-vals for a power law given x-vals
+def power_law(x_points, a, B=1):
+    # format B*x^a
+    y_vals = np.power(x_points, a)
+    y_vals = y_vals*B
+    return y_vals
+
+# Calculate a histogram that is the ratio of histograms for two sets of data
+def frac_hist(spec_ys, tot_ys, bin_size=1):
+    tot_hist, z = np.histogram(tot_ys, bin_size)
+    spec_hist, z = np.histogram(spec_ys, bin_size)
+    frac_hist, z = spec_hist.astype(float)/tot_hist.astype(float)
+
+    return tot_hist, fract_hist
+
+# Calculate the line that best fits the data using weighted least squares
+# Assumes the ideal solution is of the form y = A + Bx
+# (Input must be numpy arrays)
+def ls_fit(xs, ys, ws=None):
+
+    if ws is None:
+        n_pts = xs.shape
+        weights = np.ones(n_pts,)
+    else:
+        weights = ws
+    
+    print "weights ", weights 
+    x_sq = weights*np.power(xs,2)
+    sum_x_sq = x_sq.sum()
+    sum_x = (weights*xs).sum()
+    xy = weights*xs*ys
+    sum_xy = xy.sum()
+    sum_y = (weights*ys).sum()
+    sum_w = weights.sum()
+
+    denom = sum_w*sum_x_sq - np.power(sum_x, 2)
+    A_num = sum_x_sq*sum_y - sum_x*sum_xy
+    B_num = sum_w*sum_xy - sum_x*sum_y
+
+    A = A_num/denom
+    B = B_num/denom
+
+    return A, B
+
+    
+#def rms_fit_quality(data, model):
+
--- hist_bench.py
+++ hist_bench.py
@@ -1,22 +1,7 @@
 import numpy as np
 import pandas as pd
-
-
-def get_data(file):
-    n_header = 2
-    countries=np.genfromtxt(file, delimiter="\t", usecols=0, dtype=str,
-                            skip_header=n_header)
-    raw_data=np.genfromtxt(file, delimiter="\t", usecols=(1,2,3,4,5,6,7,8),
-                           skip_header=n_header)
-
-    # now get column names (how is there not a better way?)
-    tmp=np.genfromtxt(file, delimiter="\t", usecols=(1,2,3,4,5,6,7,8),
-                      names=True, skip_header=n_header-1)
-    col_names=tmp.dtype.names
-
-    return countries, col_names, raw_data
-
-
+import gen_fns
+   
 def calc_pursuit(raw_data, weights):
     final_vals = []
     weighted_factors = weights*raw_data
@@ -25,51 +10,6 @@ def calc_pursuit(raw_data, weights):
         final_vals.append(round(val,4))
     return final_vals
 
-def power_law(x_points, a, B=1):
-    # format B*x^a
-    y_vals = np.power(x_points, a)
-    y_vals = y_vals*B
-    return y_vals
-
-def frac_hist(spec_ys, tot_ys, bin_size=1):
-    tot_hist, z = np.histogram(tot_ys, bin_size)
-    spec_hist, z = np.histogram(spec_ys, bin_size)
-    frac_hist, z = spec_hist.astype(float)/tot_hist.astype(float)
-
-    return tot_hist, fract_hist
-
-
-# Calculate the line that best fits the data using weighted least squares
-# Assumes the ideal solution is of the form y = A + Bx
-# (Input must be numpy arrays)
-def ls_fit(xs, ys, ws=None):
-
-    if ws is None:
-        n_pts = xs.shape
-        weights = np.ones(n_pts,)
-    else:
-        weights = ws
-    
-    print "weights ", weights 
-    x_sq = weights*np.power(xs,2)
-    sum_x_sq = x_sq.sum()
-    sum_x = (weights*xs).sum()
-    xy = weights*xs*ys
-    sum_xy = xy.sum()
-    sum_y = (weights*ys).sum()
-    sum_w = weights.sum()
-
-    denom = sum_w*sum_x_sq - np.power(sum_x, 2)
-    A_num = sum_x_sq*sum_y - sum_x*sum_xy
-    B_num = sum_w*sum_xy - sum_x*sum_y
-
-    A = A_num/denom
-    B = B_num/denom
-
-    return A, B
-
-    
-#def rms_fit_quality(data, model):
 
 # Weapon states and their acquire date
 def get_nws():
@@ -78,11 +18,11 @@ def get_nws():
     nws["France"] = 1960
     nws["India"] = 1988
     nws["Israel"] = 1969
-    nws["North Korea"] = 2006
-    nws["Pakistan"] = 1987
-    nws["South Africa"] = 1979
-    nws["United Kingdom"] = 1952
-    nws["United States"] = 1945
+    nws["N Korea"] = 2006
+    nws["Pakist"] = 1987
+    nws["S Afric"] = 1979
+    nws["UK"] = 1952
+    nws["US"] = 1945
     nws["USSR"] = 1949
 
     return nws
@@ -91,8 +31,8 @@ def get_nws():
 # (from google doc: Main Prolif Spreadsheet, Dec-5-2016)
 def get_pursue():
     pursues = {}
-    pursues["Argentina"] = 1978
-    pursues["Australia"] = 1961
+    pursues["Argent"] = 1978
+    pursues["Austral"] = 1961
     pursues["Brazil"] = 1978
     pursues["China"] = 1955
     pursues["Egypt"] = 1965
@@ -102,13 +42,13 @@ def get_pursue():
     pursues["Iraq"] = 1983
     pursues["Israel"] = 1960
     pursues["Libya"] = 1970
-    pursues["North Korea"] = 1980
-    pursues["South Korea"] = 1970
-    pursues["Pakistan"] = 1972
-    pursues["South Africa"] = 1974
+    pursues["N Korea"] = 1980
+    pursues["S Korea"] = 1970
+    pursues["Pakist"] = 1972
+    pursues["S Afric"] = 1974
     pursues["Syria"] = 2000
-    pursues["United Kingdom"] = 1947
-    pursues["United States"] = 1939
+    pursues["UK"] = 1947
+    pursues["US"] = 1939
     pursues["USSR"] = 1945
 
     return pursues
@@ -144,3 +84,218 @@ def get_pes(all_countries, pes, status):
             pursue_pes.append(pes[i])
             pursue_st.append(curr_state)
     return(pursue_st, pursue_pes)
+
+
+# Read in the tsv for raw data of all countries, output a tsv with the data
+# converted into scores and appropriate columns replaced.
+def raw_to_factor_scores(infile, n_head=1):
+    from gen_fns import get_data
+    countries, col_names, raw_data = get_data(infile, n_head)
+
+    factors = np.array(["Auth", "Reactors", "Enrich", "U_Res",
+                        "Sci_Net", "Mil_Iso", "Conflict", "Mil_Sp"])
+    score_array = np.ndarray(0, factors.size + 1)
+
+    # find the relevant columns
+    auth_col = raw_data["Global_Peace_Index"]
+    
+    
+    #access a column: data[:,i]
+
+    
+# TODO: LOOP THROUGH EACH COUNTRY (MAYBE NOT!)
+# TODO: CONVERT INDIVIDUAL COLUMNS INTO FACTOR SCORES, READ BACK A NEW CSV
+ 
+
+# GDP defined in billions, mapped to a 1-10 scale
+def gdp2score(gdp_val):
+    step0 = 1
+    step1 = 25
+    step2 = 50
+    step3 = 100
+    step4 = 200
+    step5 = 500
+    step6 = 1000
+    step7 = 2000
+    step8 = 10000
+
+    score = -1
+    
+    if (gdp_val < step0):
+        score = 0
+    elif (gdp_val < step1):
+        score = 1
+    elif (gdp_val < step2):
+        score = 2
+    elif (gdp_val < step3):
+        score = 3
+    elif (gdp_val < step4):
+        score = 5
+    elif (gdp_val < step5):
+        score = 7
+    elif (gdp_val < step6):
+        score = 8.5
+    elif (gdp_val < step7):
+        score = 9
+    elif (gdp_val < step8):
+        score = 9.5
+
+    return score
+
+
+# Bilateral agreements converted to score
+# If only with non-nuclear states (npt), score is lower
+# If with nuclear states (nuclear umbrella), then only count these
+
+def agreement2score(npt, non_npt=0):
+    stepA = 2
+    stepB = 4
+    stepC = 7
+
+    score = -1
+    # if all agreements are with other non-nuclear states
+    if (non_npt == 0):
+        if (npt <= stepA):
+            score = 1
+        elif (npt <= stepB):
+            score = 2
+        elif (npt <= stepC):
+            score = 3
+    else:
+        if (non_npt <= stepA):
+            score = 6
+        elif (non_npt <= stepB):
+            score = 7
+        elif (non_npt <= stepC):
+            score = 8
+        else:
+            score = 10
+
+    return score
+
+
+# Use global peace index to convert both domestic and external stability into
+# a conflict score
+
+# *** NOT DEFINED FOR VALUES BETWEEN 2.5 and 3!!!
+def peace2score(gpi_array):
+    stepA = 1.5
+    stepB = 2
+#    step3 = 2.5
+    stepC = 3
+    stepD = 3.5
+
+    all_scores = np.array(gpi_array.size) #size(gpi_array)
+
+    for ind in range(gpi_array.size):
+        print "ind", ind
+        gpi_val = gpi_array[ind]
+        score = -1
+        
+        if (gpi_val < stepA):
+            score = 2
+        elif (gpi_val < stepB):
+            score = 4
+        elif (gpi_val < stepC):
+            score = 6
+        elif (gpi_val < stepD):
+            score = 8
+        else:
+            score = 10
+
+        print "score ", score
+        all_scores[ind] = score
+        
+    return all_scores
+
+# Fraction of GDP spent on military
+def mil2score(mil_gdp):
+    stepA = 1
+    stepB = 2
+    stepC = 3
+    stepD = 5
+
+    score = -1
+    if (mil_gdp < stepA):
+        score = 1
+    elif (mil_gdp < stepB):
+        score = 2
+    elif (mil_gdp < stepC):
+        score = 4
+    elif (mil_gdp < stepD):
+        score = 7
+    else:
+        score = 10
+
+    return score
+
+
+# Convert number of reactors to a score
+# ** DOES THIS INCLUDE RESEARCH OR ONLY COMMERCIAL?
+# ** HOW TO DEAL WITH PLANNED VS BUILT???
+def react2score(n_planned, n_built=0):
+    stepA = 3
+    stepB = 7
+
+    score = -1
+    
+    if (n_built == 0):
+        if (n_planned <= stepA):
+            score = 1
+        else:
+            score = 2
+    else:
+        if (n_built <= stepA):
+            score = 4
+        elif (n_built <= stepB):
+            score = 7
+        else:
+            score = 10
+
+    return score
+
+
+# convert network (defined by intuition as small=1, medium=2, large=3) into
+# a factor score on 1-10 scale.
+# IS THIS IMPLEMENTED CONSISTENT WITH SPREADSHEET??
+def network2score(sci_val):
+    score = -1
+
+    if (sci_val == 1):
+        score = 1
+    elif (sci_val == 2):
+        score = 5
+    else:
+        score = 10
+
+    return score
+
+
+# 
+# ** HOW DO THESE DATA SPIT OUT to prolif and non-prolif?
+# WHAT IF A COUNTRY HAS MORE THAN 6 alliacnes?
+def alliance2score(non_prolif, prolif=0):
+
+    stepA = 2
+    stepB = 4
+    stepC = 6
+    
+    score = -1
+    if (prolif == 0):
+        if (non_prolif <= stepA):
+            score = 1
+        elif (non_prolif <= stepB):
+            score = 2
+        else:
+            score = 3
+    else:
+        if (prolif <= stepA):
+            score = 5
+        elif (prolif <= stepB):
+            score = 6
+        else:
+            score = 7
+
+    return score
+
+        
